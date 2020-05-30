886d1d6e8a07aea503da9e17d2bf9d3b0928a0bb
Michael A. Davis <mdavis@inclaro.com>
2020-05-30 14:19:08 -0500

    Fixed bug with NamedTemporaryFile on Windows
    Changed NamedTemporaryFile to be manually deleted isntead of automatic. We delete it manually because otherwise on Windows it gets deleted before we the temp file is saved to the output file location. This is because on Windows, file handles with the O_TEMPORARY flag (which is set if we pass `delete=True`) are deleted as soon as they're closed.

--- xtarfile/zstd.py
+++ xtarfile/zstd.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 from tarfile import open as tarfile_open
 from tempfile import NamedTemporaryFile
+from os import remove as os_remove
 
 try:
     import zstandard
@@ -14,29 +15,47 @@ class ZstandardTarfile:
 
     @contextmanager
     def read(self, path: str, mode: str):
-        with NamedTemporaryFile() as decompressed:
-            with open(path, 'rb') as compressed:
-                zstd = zstandard.ZstdDecompressor(**self.zstd_kwargs)
-                zstd.copy_stream(compressed, decompressed)
-            decompressed.seek(0)
-            archive = tarfile_open(mode=mode, fileobj=decompressed)
-            try:
-                yield archive
-            finally:
-                archive.close()
+        try:
+            with NamedTemporaryFile(delete=False) as decompressed:
+                with open(path, 'rb') as compressed:
+                    zstd = zstandard.ZstdDecompressor(**self.zstd_kwargs)
+                    zstd.copy_stream(compressed, decompressed)
+                decompressed.seek(0)
+                archive = tarfile_open(mode=mode, fileobj=decompressed)
+                try:
+                    yield archive
+                finally:
+                    archive.close()
+        finally:
+            # We delete it manually because otherwise on Windows
+            # it gets deleted before we move it to the output file location.
+            # This is because on Windows, file handles with the O_TEMPORARY flag
+            # (which is set if we pass `delete=True`) are deleted as soon as
+            # they're closed.
+            decompressed.close()
+            os_remove(decompressed.name)
 
     @contextmanager
     def write(self, path: str, mode: str):
-        with NamedTemporaryFile() as decompressed:
-            archive = tarfile_open(decompressed.name, mode=mode)
-            try:
-                yield archive
-            finally:
-                archive.close()
-            decompressed.seek(0)
-            with open(path, 'wb') as compressed:
-                zstd = zstandard.ZstdCompressor(**self.zstd_kwargs)
-                zstd.copy_stream(decompressed, compressed)
+        try:
+            with NamedTemporaryFile(delete=False) as decompressed:
+                archive = tarfile_open(decompressed.name, mode=mode)
+                try:
+                    yield archive
+                finally:
+                    archive.close()
+                decompressed.seek(0)
+                with open(path, 'wb') as compressed:
+                    zstd = zstandard.ZstdCompressor(**self.zstd_kwargs)
+                    zstd.copy_stream(decompressed, compressed)
+        finally:
+            # We delete it manually because otherwise on Windows
+            # it gets deleted before we move it to the output file location.
+            # This is because on Windows, file handles with the O_TEMPORARY flag
+            # (which is set if we pass `delete=True`) are deleted as soon as
+            # they're closed.
+            decompressed.close()
+            os_remove(decompressed.name)
 
 
 if zstandard is None:
