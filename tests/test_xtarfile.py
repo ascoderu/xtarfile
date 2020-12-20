5d7fce0fbf4bfc2b754a01ed90dbf5c6e75fcf3d
meganomic <>
2020-12-20 10:33:09 +0100

    Use pytest correctly

--- tests/test_xtarfile.py
+++ tests/test_xtarfile.py
@@ -4,33 +4,36 @@ import pytest
 import xtarfile
 
 
-def test_compression_formats():
-    for compressor in xtarfile.xtarfile.OPEN_METH:
-        wmode = "w:" + compressor
-        rmode = "r:" + compressor
-        content = b'test content'
-        filename = 'archived-file.txt'
-        ofilename = "test.tar." + compressor
+@pytest.fixture(params=xtarfile.xtarfile.OPEN_METH)
+def _test_compression_formats(request, tmp_path):
+    wmode = "w:" + request.param
+    rmode = "r:" + request.param
+    content = b'test content'
+    filename = 'archived-file.txt'
+    ofilename = tmp_path.joinpath("test.tar." + request.param)
 
-        with xtarfile.open(name=ofilename, mode=wmode) as archive:
-            buffer1 = BytesIO()
-            buffer1.write(content)
-            buffer1.seek(0)
+    with xtarfile.open(name=ofilename, mode=wmode) as archive:
+        buffer1 = BytesIO()
+        buffer1.write(content)
+        buffer1.seek(0)
 
-            tarinfo = xtarfile.TarInfo()
-            tarinfo.size = len(content)
-            tarinfo.name = filename
-            archive.addfile(tarinfo, buffer1)
+        tarinfo = xtarfile.TarInfo()
+        tarinfo.size = len(content)
+        tarinfo.name = filename
+        archive.addfile(tarinfo, buffer1)
 
-        with xtarfile.open(name=ofilename, mode=rmode) as archive:
-            while True:
-                member = archive.next()
-                if member is None:
-                    pytest.fail('{} not found in archive'.format(filename))
-                if member.name == filename:
-                    buffer1 = archive.extractfile(member)
-                    actual_content = buffer1.read()
-                    break
+    with xtarfile.open(name=ofilename, mode=rmode) as archive:
+        while True:
+            member = archive.next()
+            if member is None:
+                pytest.fail('{} not found in archive'.format(filename))
+            if member.name == filename:
+                buffer1 = archive.extractfile(member)
+                actual_content = buffer1.read()
+                break
 
-        os_remove(ofilename)
-        assert actual_content == content
+    os_remove(ofilename)
+    assert actual_content == content
+
+def test_a(_test_compression_formats):
+    pass
