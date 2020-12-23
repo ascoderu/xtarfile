129e18a04f7d99e5ebaea452d17d2de120c8cc8a
meganomic <>
2020-12-23 20:39:28 +0100

    I rewrote entire stream mode implementation. It now actually works instead of fake working.

--- xtarfile/xtarfile.py
+++ xtarfile/xtarfile.py
@@ -1,9 +1,10 @@
 import os
 from builtins import open as _builtin_open  # This is needed because otherwise it calls the wrong 'open' function
-from tarfile import TarFile, TarInfo, _Stream, _StreamProxy, _LowLevelFile, ReadError, CompressionError, TarError
-from types import MethodType
+from tarfile import TarFile, TarInfo, _Stream, _StreamProxy, _LowLevelFile, ReadError, CompressionError, TarError, RECORDSIZE
+import struct
 
 
+# Replace _StreamProxy.getcomptype with custom version to handle our formats
 def getcomptype(self):
     if self.buf.startswith(b"\x1f\x8b\x08"):
         return "gz"
@@ -19,48 +20,120 @@ def getcomptype(self):
         return "tar"
 
 
+_StreamProxy.getcomptype = getcomptype
 
-def seekable(self):
-    return True
 
-_Stream.seekable = MethodType(seekable, _Stream)
+# Replace _Stream.__init__ with custom version to handle our formats
+def stream_init_overload(self, name, mode, comptype, fileobj, bufsize):
+    """Construct a _Stream object.
+    """
+    self._extfileobj = True
+    if fileobj is None:
+        fileobj = _LowLevelFile(name, mode)
+        self._extfileobj = False
+
+    if comptype == '*':
+        # Enable transparent compression detection for the
+        # stream interface
+        fileobj = _StreamProxy(fileobj)
+        comptype = fileobj.getcomptype()
+
+    self.name     = name or ""
+    self.mode     = mode
+    self.comptype = comptype
+    self.fileobj  = fileobj
+    self.bufsize  = bufsize
+    self.buf      = b""
+    self.pos      = 0
+    self.closed   = False
+
+    try:
+        if comptype == "gz":
+            try:
+                import zlib
+            except ImportError:
+                raise CompressionError("zlib module is not available")
+            self.zlib = zlib
+            self.crc = zlib.crc32(b"")
+            if mode == "r":
+                self._init_read_gz()
+                self.exception = zlib.error
+            else:
+                self._init_write_gz()
+
+        elif comptype == "bz2":
+            try:
+                import bz2
+            except ImportError:
+                raise CompressionError("bz2 module is not available")
+            if mode == "r":
+                self.dbuf = b""
+                self.cmp = bz2.BZ2Decompressor()
+                self.exception = OSError
+            else:
+                self.cmp = bz2.BZ2Compressor()
+
+        elif comptype == "xz":
+            try:
+                import lzma
+            except ImportError:
+                raise CompressionError("lzma module is not available")
+            if mode == "r":
+                self.dbuf = b""
+                self.cmp = lzma.LZMADecompressor()
+                self.exception = lzma.LZMAError
+            else:
+                self.cmp = lzma.LZMACompressor()
+
+        elif comptype in ("zst", "zstd"):
+            try:
+                import zstandard
+            except ImportError:
+                raise CompressionError("lzma module is not available")
+            if mode == "r":
+                self.dbuf = b""
+                dctx = zstandard.ZstdDecompressor()
+                self.cmp = dctx.decompressobj()
+                self.exception = zstandard.ZstdError
+            else:
+                dctx = zstandard.ZstdCompressor()
+                self.cmp = dctx.compressobj()
+
+        elif comptype == "lz4":
+            try:
+                from lz4.frame import LZ4FrameCompressor, LZ4FrameDecompressor
+            except ImportError:
+                raise CompressionError("lz4 module is not available")
+            if mode == "r":
+                self.dbuf = b""
+                self.cmp = LZ4FrameDecompressor()
+                self.exception = RuntimeError
+            else:
+                self.cmp = LZ4FrameCompressor()
+                self.fileobj.write((self.cmp.begin()))
+
+
+        elif comptype != "tar":
+            raise CompressionError("unknown compression type %r" % comptype)
+
+    except:
+        if not self._extfileobj:
+            self.fileobj.close()
+        self.closed = True
+        raise
+
+
+_Stream.__init__ = stream_init_overload
+
 
 class xtarfile(TarFile):
     @classmethod
-    def xopen(cls, name=None, mode="r", fileobj=None, bufsize=10240, **kwargs):
+    def xopen(cls, name=None, mode="r", fileobj=None, bufsize=RECORDSIZE, **kwargs):
         # Special handling for streams
         if "|" in mode:
             filemode, comptype = mode.split("|", 1)
-            if comptype in ("zst", "zstd", "lz4"):
-                stream = _Stream(name, filemode, "tar", fileobj, bufsize)
-                return cls.open(name, filemode + ":" + comptype, stream, bufsize, **kwargs)
-            elif comptype == "*":
-                if not fileobj:
-                    lfileobj = _LowLevelFile(name, filemode)
-                    stream_proxy = _StreamProxy(lfileobj)
-                    _StreamProxy.getcomptype = MethodType(getcomptype, stream_proxy)
-                    if stream_proxy.getcomptype() in ("gz", "bz2", "xz", "tar"):
-                        stream_proxy.close()
-                        return TarFile.open(str(name), mode, fileobj, bufsize, **kwargs)
-                    elif stream_proxy.getcomptype() in ("zst", "zstd"):
-                        import zstandard
-                        dctx = zstandard.ZstdDecompressor()
-                        fileobj = dctx.stream_reader(stream_proxy)
-                        return TarFile.open(name, "r|", fileobj, bufsize, **kwargs)
-                    elif stream_proxy.getcomptype() == "lz4":
-                        import lz4.frame as lz4
-                        fileobj = lz4.LZ4FrameFile(stream_proxy, filemode)
-                        return TarFile.open(name, "r|", fileobj, bufsize, **kwargs)
-                else:
-                    return TarFile.open(str(name), mode, fileobj, bufsize, **kwargs)
-
-                stream = _Stream(name, filemode, "tar", fileobj, bufsize)
-                return cls.open(name, filemode + ":" + comptype, stream, bufsize, **kwargs)
-            else:
-                if comptype == "gz":  # gz can't handle Path objects, need a String instead, this should be reported upstream.
-                    return TarFile.open(str(name), mode, fileobj, bufsize, **kwargs)
-                else:
-                    return TarFile.open(name, mode, fileobj, bufsize, **kwargs)
+            if comptype == 'gz':  # gz expects name to be a string. Likely an oversight. Should be reported upstream.
+                name = str(name)
 
         return cls.open(name, mode, fileobj, bufsize, **kwargs)
 
@@ -167,50 +240,7 @@ def is_tarfile(name):
     except TarError:
         return False
 
-'''
-class _StreamProxy(object):
-    """Small proxy class that enables transparent compression
-       detection for the Stream interface (mode 'r|*').
-    """
 
-    def __init__(self, fileobj):
-        self.fileobj = fileobj
-        self.buf = self.fileobj.read(10240)
-
-    def read(self, size):
-        self.read = self.fileobj.read
-        return self.buf
-
-    def getcomptype(self):
-        if self.buf.startswith(b"\x1f\x8b\x08"):
-            return "gz"
-        elif self.buf[0:3] == b"BZh" and self.buf[4:10] == b"1AY&SY":
-            return "bz2"
-        elif self.buf.startswith((b"\x5d\x00\x00\x80", b"\xfd7zXZ")):
-            return "xz"
-        elif self.buf.startswith(b"\x28\xB5\x2F\xFD"):
-            return "zst"
-        elif self.buf.startswith(b"\x04\x22\x4D\x18"):
-            return "lz4"
-        else:
-            return "tar"
-
-    def close(self):
-        self.fileobj.close()
-'''
-
-# When extending, use lz4open as a base. These are the important things to note.
-# Change "import lz4.frame as lz4" to the compression package of your choice. Change the relevant ImportError
-# Change compresslevel/error to whatever is supported by your format.
-# This is the bit that actually does something useful.
-# fileobj = lz4.LZ4FrameFile(fileobj or name, mode, compression_level=compresslevel)
-# As long as fileobj is a valid 'file object' when you hit "t = cls.taropen(name, mode, fileobj, **kwargs)", it should Just Work™
-# Add the error your format raises when it fails to open a file for reading to "except (OSError, EOFError, RuntimeError):"
-# RuntimeError is what lz4 uses and should be removed. Unless that is what your format uses ofcourse :D
-# Register the function down below, format is "file extension" : "func".
-# You don't have to use the file extension. The 'key' part of the dictionary is used when calling xtarfile.open with mode="w:key" or similar
-# See https://docs.python.org/3/library/tarfile.html for more information about modes
-# And lastly, add your format to 'if comptype in ("zst", "zstd", "lz4"):' in xopen()
 xtarfile.OPEN_METH.update({"zst": "zstopen",
                            "zstd": "zstopen",
                            "lz4": "lz4open"})
