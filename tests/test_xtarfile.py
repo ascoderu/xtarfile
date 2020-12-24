c4ed2a97aff600cb3561e68e857bad1df1852476
meganomic <>
2020-12-24 07:47:54 +0100

    Create xfail for 'gz' because it has a bug in the library. Will fail tests if bug is fixed in the future.

--- tests/test_xtarfile.py
+++ tests/test_xtarfile.py
@@ -4,9 +4,14 @@ import xtarfile
 
 
 testfiles = dict()
+STREAM_OPEN_METHODS = list()
 content = b'test content'
 filename = 'archived-file.txt'
-
+for method in xtarfile.xtarfile.OPEN_METH.keys():
+    if method == 'gz':
+        STREAM_OPEN_METHODS.append(pytest.param(method, marks=pytest.mark.xfail(reason="compression GZ doesn't support PathLike objects and thus creates no files", strict=True)))
+    else:
+        STREAM_OPEN_METHODS.append(method)
 
 @pytest.fixture(params=xtarfile.xtarfile.OPEN_METH)
 def _test_xwriting_filedoesntexist(request, tmp_path):
@@ -80,66 +85,60 @@ def _test_reading(request):
             pytest.xfail("gz doesn't support PathLike objects, so no files were created.")
 
 
-@pytest.fixture(params=xtarfile.xtarfile.OPEN_METH)
+@pytest.fixture(params=STREAM_OPEN_METHODS)
 def _test_stream_mode_writing(request, tmp_path):
     ofilename = tmp_path.joinpath("test.tar." + request.param)
-    try:
-        with xtarfile.open(name=ofilename, mode="w|" + request.param) as archive:
-            buffer1 = BytesIO()
-            buffer1.write(content)
-            buffer1.seek(0)
+    with xtarfile.open(name=ofilename, mode="w|" + request.param) as archive:
+        buffer1 = BytesIO()
+        buffer1.write(content)
+        buffer1.seek(0)
 
-            tarinfo = xtarfile.TarInfo()
-            tarinfo.size = len(content)
-            tarinfo.name = filename
-            archive.addfile(tarinfo, buffer1)
+        tarinfo = xtarfile.TarInfo()
+        tarinfo.size = len(content)
+        tarinfo.name = filename
+        archive.addfile(tarinfo, buffer1)
 
-        return ofilename, request.param
-    except AttributeError:
-        if request.param == "gz":
-            pytest.xfail("gz doesn't support PathLike objects")
+    return ofilename, request.param
 
 
-@pytest.fixture(params=xtarfile.xtarfile.OPEN_METH)
+@pytest.fixture(params=STREAM_OPEN_METHODS)
 def _test_stream_mode_reading(request):
-    try:
-        with xtarfile.open(name=testfiles[request.param], mode="r|" + request.param) as archive:
-            while True:
-                member = archive.next()
-                if member is None:
-                    pytest.fail('{} not found in archive'.format(filename))
-                if member.name == filename:
-                    buffer1 = archive.extractfile(member)
-                    actual_content = buffer1.read()
-                    break
+    with xtarfile.open(name=testfiles[request.param], mode="r|" + request.param) as archive:
+        while True:
+            member = archive.next()
+            if member is None:
+                pytest.fail('{} not found in archive'.format(filename))
+            if member.name == filename:
+                buffer1 = archive.extractfile(member)
+                actual_content = buffer1.read()
+                break
 
-        return actual_content
-    except KeyError:
-        if request.param == "gz":
-            pytest.xfail("gz doesn't support PathLike objects, so no files were created.")
+    return actual_content
 
 
-@pytest.fixture(params=xtarfile.xtarfile.OPEN_METH)
+
+@pytest.fixture(params=STREAM_OPEN_METHODS)
 def _test_stream_mode_reading_auto(request):
-    try:
-        with xtarfile.open(name=testfiles[request.param], mode="r|*") as archive:
-            while True:
-                member = archive.next()
-                if member is None:
-                    pytest.fail('{} not found in archive'.format(filename))
-                if member.name == filename:
-                    buffer1 = archive.extractfile(member)
-                    actual_content = buffer1.read()
-                    break
+    with xtarfile.open(name=testfiles[request.param], mode="r|*") as archive:
+        while True:
+            member = archive.next()
+            if member is None:
+                pytest.fail('{} not found in archive'.format(filename))
+            if member.name == filename:
+                buffer1 = archive.extractfile(member)
+                actual_content = buffer1.read()
+                break
 
-        return actual_content
-    except KeyError:
-        if request.param == "gz":
-            pytest.xfail("gz doesn't support PathLike objects, so no files were created.")
+    return actual_content
 
 
 @pytest.fixture(params=xtarfile.xtarfile.OPEN_METH)
 def _test_import_is_tarfile(request):
+    return xtarfile.is_tarfile(testfiles[request.param])
+
+
+@pytest.fixture(params=STREAM_OPEN_METHODS)
+def _test_import_is_tarfile_after_stream(request):
     return xtarfile.is_tarfile(testfiles[request.param])
 
 
@@ -148,6 +147,10 @@ def test_stream_mode_writing(_test_stream_mode_writing):
     filename, OPEN_METH = _test_stream_mode_writing
     assert filename.is_file() == True
     testfiles.update({OPEN_METH: filename})
+
+
+def test_import_is_tarfile_after_stream(_test_import_is_tarfile_after_stream):
+    assert _test_import_is_tarfile_after_stream == True
 
 
 def test_reading_stream_created_files(_test_reading):
@@ -171,12 +174,12 @@ def test_writing(_test_writing):
     testfiles.update({OPEN_METH: filename})
 
 
+def test_import_is_tarfile_after_writing(_test_import_is_tarfile):
+    assert _test_import_is_tarfile == True
+
+
 def test_reading(_test_reading):
     assert content == _test_reading
-
-
-def test_import_is_tarfile(_test_import_is_tarfile):
-    assert _test_import_is_tarfile == True
 
 
 # Make sure opening with 'x' works as expected
@@ -191,6 +194,10 @@ def test_xwriting_filedoesntexist(_test_xwriting_filedoesntexist):
     filename, OPEN_METH = _test_xwriting_filedoesntexist
     assert filename.is_file() == True
     testfiles.update({OPEN_METH: filename})
+
+
+def test_import_is_tarfile_after_xwriting(_test_import_is_tarfile):
+    assert _test_import_is_tarfile == True
 
 
 def test_reading_after_xwrite_2(_test_reading):
